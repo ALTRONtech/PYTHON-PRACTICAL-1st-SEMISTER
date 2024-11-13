6ff16a57745e93175d6a3e4bd4a7680c1ee9eb38
Dipesh Patne <dipeshpatne71@gmail.com>
2024-11-13 23:55:34 +0530

    Add files via upload

---
+++ PYTHON PRACTICAL 1st SEMISTER/PRACTICAL NO 1/6_Break Statement.py
@@ -0,0 +1,11 @@
+# The break statement exits the loop immediately, regardless of the loop condition.
+for i in range(10):
+    if i==5:
+        break
+    print(i)
+# output
+    # 0
+    # 1
+    # 2
+    # 3
+    # 4
---
+++ PYTHON PRACTICAL 1st SEMISTER/PRACTICAL NO 1/7_ Continue Statement.py
@@ -0,0 +1,1 @@
+# The continue statement skips the current iteration and moves to the next one.
