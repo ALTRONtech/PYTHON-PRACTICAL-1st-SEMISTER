755476cd117babce80e9830717042af5095adc5f
Dipesh Patne <dipeshpatne71@gmail.com>
2024-11-14 14:17:57 +0530

    add new

--- PYTHON PRACTICAL 1st SEMISTER/PRACTICAL NO 1/7_ Continue Statement.py
+++ PYTHON PRACTICAL 1st SEMISTER/PRACTICAL NO 1/7_ Continue Statement.py
@@ -1,1 +1,10 @@
 # The continue statement skips the current iteration and moves to the next one.
+for i in range(5):
+    if i == 2:
+        continue
+    print(i)
+ #output
+# 0
+# 1
+# 3
+# 4
---
+++ PYTHON PRACTICAL 1st SEMISTER/PRACTICAL NO 1/8_Pass Statement.py
@@ -0,0 +1,8 @@
+# The pass statement is a null operation. It does nothing and is often used as a placeholder.
+x = 10
+if x > 5:
+    pass  # Placeholder for future code
+else:
+    print("x is not greater than 5")
+    
+# Output: (No output since pass does nothing)
